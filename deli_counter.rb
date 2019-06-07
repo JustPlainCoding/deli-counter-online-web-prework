c18f91bcab0bfb7338e33c66b10bc57566af295c
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-07 15:46:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line(katz_deli)
+  current_line = 
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else
+    katz_deli.each_with_index do |person, index|
+      if index == 0
+         
+  end
+end
+
+def take_a_number(katz_deli)
+end
+
+def now_serving(katz_deli)
+end
